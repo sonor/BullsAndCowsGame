be27347aac7367d6dbeca63993c44fbc208ce465
Vladimir Zhuvak <grast8@gmail.com>
2020-01-05 02:06:47 +0200

    terminal welcome message changed

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,14 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+	PrintLine("Welcome to Bull Cows Game!");
+	PrintLine("Please input something and press ENTER...");
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+	ClearScreen();
+
 
 }
